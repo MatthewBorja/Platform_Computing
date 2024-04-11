ed4a86e772ae24dfeabb83dea001721fc65dfbd7
Matthew Borja <matthewborja97@gmail.com>
2024-04-10 20:01:59 -0700

    Added Image User

---
+++ Assignment_4_Generate_Users/Image_User.js
@@ -0,0 +1,61 @@
+const {By, Builder, Browser} = require('selenium-webdriver');
+const assert = require("assert");
+
+function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+async function countElem(driver, tag){
+    try {
+        let elements = await driver.findElements(By.tagName(tag));
+        //Return the count of elements in website.
+        return elements.length;
+    } catch (error) {
+        //Handle any errors that might occur.
+        console.error("Error in finding elements", error);
+        return false;
+    }
+};
+
+(async function keywordUser(){
+    let driver;
+    let reward_time = 10000;
+    let total_reward_time = 0;
+    //let url = 'http://localhost:3000';
+    let url = 'https://devdocs.io/cpp/';
+    let tag = 'img';
+    let count = 0;
+    console.log("Website: ", url, " Tag:", tag);
+    try {
+        //Start the session.
+        driver = await new Builder().forBrowser(Browser.CHROME).build();
+        //Take action on browser.
+        await driver.get(url);
+        
+        //Request browser information.
+        let title = await driver.getTitle();
+
+        //Establish Waiting Strategy.
+        await driver.manage().setTimeouts({implicit:500});
+
+        //Find an element.
+        count = await countElem(driver, tag);
+        for (i = 0; i < count; i++){
+            total_reward_time += reward_time;
+            wait(reward_time);
+        }
+        /*if (count > 0){
+            total_reward_time += reward_time;
+            wait(reward_time);
+        }*/
+        //Take action on element.
+        //Request element information.
+        console.log("Presence Time: ", total_reward_time/1000, " seconds.");
+    } catch (e) {
+        console.log(e);
+    } finally {
+        //End the session.
+        await driver.quit();
+    }
+
+})();
--- Assignment_4_Generate_Users/README.md
+++ Assignment_4_Generate_Users/README.md
@@ -10,7 +10,7 @@ The following **required** features are completed:
 
 - [X] Organize Github folders (2 pts)
 - [X] Create a user with affinity towards keyword(s) and verify that it works (3 pts)
-- [ ] Create a user with affinity towards image(s) and verify that it works (3 pts)
+- [X] Create a user with affinity towards image(s) and verify that it works (3 pts)
 - [ ] Create a user with affinity towards links and verify that it works (3 pts)
 - [ ] Describe a user with unique affinities (1 pt)
 - [ ] Create a user with unique affinities and verify that it works (3 pts)
@@ -23,6 +23,7 @@ The following **bonus** features are implemented:
 ## Screenshot and/or Video Walkthrough
 
 <img src="https://github.com/MatthewBorja/Platform_Computing/blob/main/Assignment_4_Generate_Users/Images/keyword.PNG?raw=true" title='Keyword User' width='' alt='Keyword User' />
+<img src="" title='Image User' width='' alt='Image User' />
 
 
 ## Notes:
